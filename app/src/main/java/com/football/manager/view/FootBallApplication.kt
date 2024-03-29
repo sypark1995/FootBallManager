95040d0621955f40f7274b51f011690c32d90ff3
sypark1995 <115516297+sypark1995@users.noreply.github.com>
2024-03-29 14:43:59 +0900

    feat : application hilt 어노테이션 추가

--- app/src/main/java/com/football/manager/view/FootBallApplication.kt
+++ app/src/main/java/com/football/manager/view/FootBallApplication.kt
@@ -1,8 +1,10 @@
 package com.football.manager.view
 
 import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
 
+@HiltAndroidApp
 class FootBallApplication: Application() {
     override fun onCreate() {
         super.onCreate()
