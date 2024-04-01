34403e4f892fc90310eb1bf382b146dbe08902d7
sypark1995 <115516297+sypark1995@users.noreply.github.com>
2024-04-01 15:38:35 +0900

    refactor : remove header

--- core-network/src/main/java/com/football/manager/core_network/interceptor/HttpRequestInterceptor.kt
+++ core-network/src/main/java/com/football/manager/core_network/interceptor/HttpRequestInterceptor.kt
@@ -8,13 +8,8 @@ import timber.log.Timber
 internal class HttpRequestInterceptor : Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
         val originalRequest = chain.request()
-        originalRequest.run {
-            newBuilder().apply {
-                header("x-rapidapi-key", BuildConfig.BUILD_TYPE)
-                header("x-rapidapi-host", "v3.football.api-sports.io")
-                url(originalRequest.url).build()
-            }
-        }
+        originalRequest.newBuilder()
+            .build()
         Timber.e(originalRequest.toString())
         return chain.proceed(originalRequest)
     }
