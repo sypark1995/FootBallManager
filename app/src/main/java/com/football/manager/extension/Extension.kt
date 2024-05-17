a0a1f2ed291ed7016a4581ff4d5ec409fc82495e
sypark1995 <115516297+sypark1995@users.noreply.github.com>
2024-05-17 16:04:06 +0900

    feat : view extension add

--- app/src/main/java/com/football/manager/extension/Extension.kt
+++ app/src/main/java/com/football/manager/extension/Extension.kt
@@ -1,7 +1,22 @@
 package com.football.manager.extension
 
+import android.view.View
+
 object StringUtil {
     fun longToString(value: Long):String {
         return value.toString()
     }
+}
+
+
+fun View.hide() {
+    visibility = View.GONE
+}
+
+fun View.show() {
+    visibility = View.VISIBLE
+}
+
+fun View.invisible() {
+    visibility = View.INVISIBLE
 }
