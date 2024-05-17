943378ee8cc03a4c8062c5b483116124119fea27
sypark1995 <115516297+sypark1995@users.noreply.github.com>
2024-05-17 16:44:39 +0900

    refactor : hilt add

--- app/src/main/java/com/football/manager/view/activity/IntroActivity.kt
+++ app/src/main/java/com/football/manager/view/activity/IntroActivity.kt
@@ -6,9 +6,11 @@ import androidx.lifecycle.lifecycleScope
 import com.football.manager.view.base.BaseActivity
 import com.football.manager.R
 import com.football.manager.databinding.ActivityIntroBinding
+import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
+@AndroidEntryPoint
 class IntroActivity : BaseActivity<ActivityIntroBinding>(R.layout.activity_intro) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
